230be9c325d7d0390da165bb3a89724044e13ef0
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-19 14:06:14 -0600

    Fix object and function syntax in index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 let Avangeline = {
-    'age': 0,
+    name = "Avangeline",
+    age: 0,
+
 };
 
-function celebrateBirthday(Avangeline) = {
-  Avangeline.age = Avangeline.age + 1;
-  return Avangeline;
+function celebrateBirthday(person) = {
+  person.age = person.age + 1;
+  return person;
 }
 
 
 //should create a person object that has an age of 0 (zero)
 
 //should create a function named celebrateBirthday that takes a person, updates their age by one year and returns the person
+
